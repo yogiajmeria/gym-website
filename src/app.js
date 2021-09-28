6adbd825d64c8c9b59795d2f17f8416b904247ce
yogi <yogiajmeria@gmail.com>
2021-09-28 23:24:36 +0530

    inital

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use(express.static(staticPath));
 app.get("/" ,(req,res)=>{
   res.render('login.hbs')
 })
+
 app.post('Contact', (req,res)=>{
 let mydata =new contact(req.body);
 mydata.save().then((req,res)=>{
